8e80c7c92891cce4bb5f39d8dc77ce49d8811509
umarkhonovv <you@example.com>
2025-08-17 12:39:24 +0500

    docker added

--- SSDProject/WebApiB/Program.cs
+++ SSDProject/WebApiB/Program.cs
@@ -1,4 +1,9 @@
 
+using MassTransit;
+using WebApiB.Data;
+using WebApiB.Rabbit;
+using WebApiB.Services;
+
 namespace WebApiB
 {
     public class Program
@@ -6,17 +11,40 @@ namespace WebApiB
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            var config = builder.Configuration;
 
-            // Add services to the container.
+            builder.Services.AddDbContext<AppDbContext>(options =>
+            options.UseNpgsql(config.GetConnectionString("PostgresConnection")));
+
+            builder.Services.AddScoped<IUserService, UserService>();
+
+            builder.Services.AddMassTransit(x =>
+            {
+                x.AddConsumer<RabbitConsumer>();
+
+                x.UsingRabbitMq((ctx, cfg) =>
+                {
+                    cfg.Host("host.docker.internal", "/", h =>
+                    {
+                        h.Username("guest");
+                        h.Password("guest");
+                    });
+
+                    cfg.ReceiveEndpoint("user-queue", e =>
+                    {
+                        e.ConfigureConsumer<RabbitConsumer>(ctx);
+                    });
+                });
+            });
+
+            builder.Services.AddMassTransitHostedService();
 
             builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
